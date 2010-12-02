68979eb94b00632b1188fac7e877789b9d9401ae
toddhuish <toddhuish@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-02 19:03:15 +0000

    release for 1.1.3
    git-svn-id: https://plugins.svn.wordpress.org/wpeventticketing/trunk@318139 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ticketing.php
+++ ticketing.php
@@ -4,7 +4,7 @@ Plugin Name: WP Event Ticketing
 Plugin URI: http://9seeds.com/plugins/
 Description: The WP Event Ticketing plugin makes it easy to sell and manage tickets for your event.
 Author: 9seeds.com
-Version: 1.1.2
+Version: 1.1.3
 Author URI: http://9seeds.com/
 */
 
